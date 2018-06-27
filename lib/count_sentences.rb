f97d4a5c9179616caad76b8cadfd7f025bc2bfca
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 20:12:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences = sentences.reject { |item| item.empty? }
+    sentences.length
   end
 end
+
+#str = String.new("how many sentences. 1,2,3?? ?")
+#puts str.count_sentences
